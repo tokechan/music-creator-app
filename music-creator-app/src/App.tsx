62e223bb4936e63ecf63a1102114180a3eb9cfac
tokec <fleatoke@gmail.com>
2025-10-08 19:07:34 +0900

    Update dependencies in package.json and package-lock.json, add path aliasing in tsconfig files, and enhance styling in App.tsx and index.css with new CSS variables and animations.

--- music-creator-app/src/App.tsx
+++ music-creator-app/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   
   return (  
     <div>
       <h1 className='text-3xl  font-bold text-gray-500 mb-4 underline'>Generate Music App</h1>
+      <Button variant="outline" className='bg-blue-500 text-white'>Generate</Button>
     </div>
   )
 }
