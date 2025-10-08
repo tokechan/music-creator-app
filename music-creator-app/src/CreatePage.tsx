ce371ee3ee176c0a72f48e7e1c2434c85893d552
tokec <fleatoke@gmail.com>
2025-10-08 19:15:26 +0900

    Implement React Router for navigation by adding CreatePage component and updating main.tsx to define routes.

---
+++ music-creator-app/src/CreatePage.tsx
@@ -0,0 +1,7 @@
+function CreatePage() {
+    return <div>
+        <h1 className='text-3xl font-bold text-gray-500 mb-4 underline'>Music Creation Page</h1>
+    </div>
+}
+
+export default CreatePage;
